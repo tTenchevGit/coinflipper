e529da6f693e5c8d77c8c43806d46db64fb5bf30
Toni Tenchev <tonitenchev93@gmail.com>
2024-02-23 06:55:13 +0000

    update flip function

--- script.js
+++ script.js
@@ -9,15 +9,12 @@ let tailsCount = 0;
 flipButton.addEventListener("click", flipCoin);
 
 function flipCoin() {
-    coinElement.classList.add("flipping"); // Add the flipping class here
+    coinElement.classList.add("flipping"); 
     
-    // Generate random outcome
     const outcome = Math.random() < 0.5 ? "Heads" : "Tails";
     
-    // Display outcome
     outcomeDisplay.textContent = outcome;
     
-    // Update statistics
     if (outcome === "Heads") {
         headsCount++;
     } else {
@@ -25,16 +22,13 @@ function flipCoin() {
     }
     updateStats();
 
-    // Generate random color for button and outcome text
     const randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
     
     flipButton.style.backgroundColor = randomColor;
     outcomeDisplay.style.color = randomColor;
 
-    // Display blinking text for both "Heads" and "Tails"
     displayBlinkingText(outcome);
 
-    // Remove the flipping class after the animation finishes
     setTimeout(() => {
         coinElement.classList.remove("flipping");
     }, 1000);
@@ -49,7 +43,6 @@ function displayBlinkingText(outcome) {
     blinkingText.style.color = outcomeDisplay.style.color;
     document.body.appendChild(blinkingText);
 
-    // Remove blinking text after 2 seconds
     setTimeout(() => {
         blinkingText.remove();
     }, 2000);
